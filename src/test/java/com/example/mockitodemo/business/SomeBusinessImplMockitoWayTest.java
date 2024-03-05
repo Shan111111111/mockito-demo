8b69aa8b924978e99815f441a2bcd7d9747c2385
Shantesh <shanthu1804@gmail.com>
2024-03-06 02:05:01 +0530

    used annotation with @Mock to achieve the same before stuff/functionality and it is more easy as well

--- src/test/java/com/example/mockitodemo/business/SomeBusinessImplMockitoWayTest.java
+++ src/test/java/com/example/mockitodemo/business/SomeBusinessImplMockitoWayTest.java
@@ -5,19 +5,24 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockitoWayTest {
+	@Mock
+	private DataService dataService;
 	
+	@InjectMocks
+	private SomeBusinessImpl someBusinessImpl;
 
 	@Test
 	void findTheGreatestFromAllData_basicScenario() {
 		 
-		DataService mock = mock(DataService.class);
-		when(mock.retrieveAllData()).thenReturn(new int[] {25,15,5});
-		
-		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(mock );
-		int result = someBusinessImpl.findTheGreatestFromAllData();
-		assertEquals(25, result);
+		when(dataService.retrieveAllData()).thenReturn(new int[] {25,15,5});
+		assertEquals(25, someBusinessImpl.findTheGreatestFromAllData());
 	}
 	
 	
