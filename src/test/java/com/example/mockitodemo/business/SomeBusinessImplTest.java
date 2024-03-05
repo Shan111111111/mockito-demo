0b8f02c86ec7219c5443f5ab1b8118835938db8f
Shantesh <shanthu1804@gmail.com>
2024-03-06 01:51:02 +0530

    if need to test many scenarios then we need to create that many stubs or classes so headache to maintain code

--- src/test/java/com/example/mockitodemo/business/SomeBusinessImplTest.java
+++ src/test/java/com/example/mockitodemo/business/SomeBusinessImplTest.java
@@ -10,18 +10,42 @@ public class SomeBusinessImplTest {
 	@Test
 	void test() {
 		 
-		DataService dataServiceStub = new DataServiceStub();
+		DataService dataServiceStub = new DataServiceStubWithThreeMemberArray();
 		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceStub );
 		int result = someBusinessImpl.findTheGreatestFromAllData();
 		assertEquals(25, result);
 	}
+	
+	@Test
+	void testfindTheGreatestWithOneValue() {
+		 
+		DataService dataServiceStub = new DataServiceStub2WithOneValue();
+		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceStub );
+		int result = someBusinessImpl.findTheGreatestFromAllData();
+		assertEquals(35, result);
+	}
 }
 
-class DataServiceStub implements DataService{
+class DataServiceStubWithThreeMemberArray implements DataService {
 
 	@Override
 	public int[] retrieveAllData() {
-		return new int[] {25, 15, 5};
+		return new int[] { 25, 15, 5 };
 	}
-	
+}
+
+class DataServiceStub2WithOneValue implements DataService {
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] { 35 };
+	}
+}
+
+class DataServiceStub3WithEmptyArray implements DataService {
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {};
+	}
 }
