9000059208b264708ff0cff978f583245b62e566
Shantesh <shanthu1804@gmail.com>
2024-03-06 01:57:30 +0530

    by Mockito mock way of testing compared to stub way which is easy and with more beneficial

---
+++ src/test/java/com/example/mockitodemo/business/SomeBusinessImplMockitoWayTest.java
@@ -0,0 +1,24 @@
+package com.example.mockitodemo.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+
+public class SomeBusinessImplMockitoWayTest {
+	
+
+	@Test
+	void findTheGreatestFromAllData_basicScenario() {
+		 
+		DataService mock = mock(DataService.class);
+		when(mock.retrieveAllData()).thenReturn(new int[] {25,15,5});
+		
+		SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(mock );
+		int result = someBusinessImpl.findTheGreatestFromAllData();
+		assertEquals(25, result);
+	}
+	
+
+}
